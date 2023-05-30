9b5b740301db9c4f242a2c4b223601b1914eb32e
alan <alan@g1project.net>
2023-05-30 12:31:14 +0900

    study: exception and stack trace 6

--- src/main/java/org/alan/javabasic/learningexception/ExceptionMain4.java
+++ src/main/java/org/alan/javabasic/learningexception/ExceptionMain4.java
@@ -3,7 +3,8 @@ package org.alan.javabasic.learningexception;
 public class ExceptionMain4 {
 
     public static void main(String[] args) {
-        String runtimeExceptionMessage = "RuntimeException is thrown by main method.";
+        String runtimeExceptionDetailMessage = makeThrownMessage(RuntimeException.class, "main");
+
         try {
             System.out.println("main method called.");
             ExceptionMain4 exceptionMain4 = new ExceptionMain4();
@@ -11,66 +12,101 @@ public class ExceptionMain4 {
             System.out.println("Program ended.");
         }
 
-//        catch (ExceptionA exceptionA) {
-//            System.err.println("ExceptionA caught by main.");
-//            throw new RuntimeException(runtimeExceptionMessage, exceptionA);
-//        }
-
-        catch (ExceptionB exceptionB) {
-            System.err.println("ExceptionB caught by main.");
-            throw new RuntimeException(runtimeExceptionMessage, exceptionB);
+        catch (ExceptionA exceptionA) {
+            System.err.println(makeCaughtMessage(ExceptionA.class, "main"));
+            throw new RuntimeException(runtimeExceptionDetailMessage, exceptionA);
         }
 
-//        catch (NullPointerException nullPointerException) {
-//            System.err.println("NullPointerException caught by main.");
-//            runtimeExceptionMessage = "RuntimeException is thrown by main method.";
-//            throw new RuntimeException(runtimeExceptionMessage, nullPointerException);
-//        }
+        catch (ExceptionB exceptionB) {
+            System.err.println(makeCaughtMessage(ExceptionB.class, "main"));
+            throw new RuntimeException(runtimeExceptionDetailMessage, exceptionB);
+        }
+
+        catch (ExceptionC exceptionC) {
+            System.err.println(makeCaughtMessage(ExceptionC.class, "main"));
+            throw new RuntimeException(runtimeExceptionDetailMessage, exceptionC);
+        }
+
+        catch (NullPointerException nullPointerException) {
+            System.err.println(makeCaughtMessage(NullPointerException.class, "main"));
+            throw new RuntimeException(runtimeExceptionDetailMessage, nullPointerException);
+        }
 
     }
 
     private void methodA() {
+        String methodA = "methodA";
+        String exceptionADetailMessage = makeThrownMessage(ExceptionA.class, methodA);
+
         try {
             System.out.println("methodA called.");
             methodB();
         }
 
         catch (ExceptionB exceptionB) {
-            System.err.println("ExceptionB caught by methodA.");
-            String exceptionAMessage = "ExceptionA is thrown by methodA!";
-            throw new ExceptionA(exceptionAMessage, exceptionB);
+            System.err.println(makeCaughtMessage(ExceptionB.class, methodA));
+            throw new ExceptionA(exceptionADetailMessage, exceptionB);
         }
 
-        catch (NullPointerException e) {
-            System.err.println("NullPointerException caught by methodA.");
-            String exceptionAMessage = "ExceptionA is thrown by methodA.";
-            throw new ExceptionA(exceptionAMessage);
+        catch (ExceptionC exceptionC) {
+            System.err.println(makeCaughtMessage(ExceptionC.class, methodA));
+            throw new ExceptionA(exceptionADetailMessage, exceptionC);
+        }
+
+        catch (NullPointerException nullPointerException) {
+            System.err.println(makeCaughtMessage(NullPointerException.class, methodA));
+            throw new ExceptionA(exceptionADetailMessage, nullPointerException);
         }
 
     }
 
     private void methodB() {
+        String methodB = "methodB";
+        String exceptionBDetailMessage = makeThrownMessage(ExceptionB.class, methodB);
+
         try {
             System.out.println("methodB called.");
             methodC();
         }
 
         catch (ExceptionC exceptionC) {
-            System.err.println("ExceptionC caught by methodB.");
-            String exceptionBMessage = "ExceptionB is thrown by methodB!";
-            throw new ExceptionB(exceptionBMessage, exceptionC);
+            System.err.println(makeCaughtMessage(ExceptionC.class, methodB));
+            throw new ExceptionB(exceptionBDetailMessage, exceptionC);
+        }
+
+        catch (NullPointerException nullPointerException) {
+            System.err.println(makeCaughtMessage(NullPointerException.class, methodB));
+            throw new ExceptionB(exceptionBDetailMessage, nullPointerException);
         }
     }
 
     private void methodC() {
-        System.out.println("methodC called.");
-        methodNullPointer();
+        String methodC = "methodC";
+        String exceptionCDetailMessage = makeThrownMessage(ExceptionC.class, methodC);
+
+        try {
+            System.out.println("methodC called.");
+            methodNullPointer();
+        }
+
+        catch (NullPointerException nullPointerException) {
+            System.err.println(makeCaughtMessage(NullPointerException.class, methodC));
+            throw new ExceptionC(exceptionCDetailMessage, nullPointerException);
+        }
     }
 
     private void methodNullPointer() {
         System.out.println("methodNullPointer called.");
         String nullPointer = null;
         String upper = nullPointer.toUpperCase();
+    }
+
+    private static String makeThrownMessage(Class<?> exceptionName, String by) {
+        return exceptionName + " is thrown by " + by + ".";
+    }
+
+    private static String makeCaughtMessage(Class<?> exceptionName, String by) {
+        return exceptionName + " is caught by " + by + ".";
     }
 
 }
