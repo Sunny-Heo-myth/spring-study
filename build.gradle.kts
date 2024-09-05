852536a560bd783940df1ab6efc46ed7177e0309
alan <hsymyth@gmail.com>
2024-09-05 18:12:27 +0900

    feat: upgrade to java 21 with gradle 8.8

---
+++ build.gradle.kts
@@ -0,0 +1,42 @@
+plugins {
+    java
+    id("io.spring.dependency-management") version "1.1.0"
+    id("org.springframework.boot") version "3.0.2"
+}
+
+group = "org.alan"
+version = "1.0.0"
+java.sourceCompatibility = JavaVersion.VERSION_21
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    // spring boot starter
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    // database & jpa
+    runtimeOnly("org.postgresql:postgresql")
+    implementation("mysql:mysql-connector-java")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.hibernate.validator:hibernate-validator")
+
+    // development tool
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+
+    // lombok
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
